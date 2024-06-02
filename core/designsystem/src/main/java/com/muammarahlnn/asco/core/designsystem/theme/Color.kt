5a5b57dc053e13dcf14a2745254b8660bb92d18a
muammarahlnn <21muammarahlan@gmail.com>
2024-06-02 23:59:15 +0800

    feat(designsystem): add base colors

---
+++ core/designsystem/src/main/java/com/muammarahlnn/asco/core/designsystem/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.muammarahlnn.asco.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * @Author Muammar Ahlan Abimanyu
+ * @File Color, 02/06/2024 23.52
+ */
+
+internal val Black = Color(0xFF01000D)
+internal val White = Color(0xFFF2F6FE)
+internal val Gray = Color(0xFF92949F)
+internal val DarkestPurple = Color(0xFF1E0059)
+internal val DarkerPurple = Color(0xFF3E2484)
+internal val Purple = Color(0xFF744BE4)
+internal val LighterPurple = Color(0xFF8172F4)
+internal val LightestPurple = Color(0xFFD0BDFF)
+internal val WhitePurple = Color(0xFFF1ECFF)
+internal val Blue = Color(0xFFAED1F2)
+internal val DarkBlue = Color(0xFF85A8DD)
+internal val Pink = Color(0xFFF2AECF)
+internal val DarkPink = Color(0xFFDD85AF)
+internal val Orange = Color(0xFFFFB37F)
+internal val DarkOrange = Color(0xFFFF9A57)
+internal val Green = Color(0xFFDD85AF)
+internal val DarkGreen = Color(0xFFDD85AF)
