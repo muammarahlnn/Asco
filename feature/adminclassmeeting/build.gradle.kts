a16ee6333906edf3dfb300c961978b64eade9017
muammarahlnn <21muammarahlan@gmail.com>
2024-06-30 02:23:24 +0800

    feat(adminclassmeeting): add AdminClassMeeting module

---
+++ feature/adminclassmeeting/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("asco.android.feature")
+    id("asco.android.library.compose")
+}
+
+android {
+    namespace = "com.muammarahlnn.asco.feature.adminclassmeeting"
+}
+
+dependencies {
+    implementation(libs.androidx.activity.compose)
+}
