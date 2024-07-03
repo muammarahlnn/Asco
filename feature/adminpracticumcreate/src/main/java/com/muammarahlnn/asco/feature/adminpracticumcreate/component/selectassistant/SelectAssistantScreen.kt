8143044775c3f6c0e1e213e669c575bd51182560
muammarahlnn <21muammarahlan@gmail.com>
2024-07-04 03:14:38 +0800

    feat(core:ui): make SelectMembersScreen reusable

--- feature/adminpracticumcreate/src/main/java/com/muammarahlnn/asco/feature/adminpracticumcreate/component/selectassistant/SelectAssistantScreen.kt
+++ feature/adminpracticumcreate/src/main/java/com/muammarahlnn/asco/feature/adminpracticumcreate/component/selectassistant/SelectAssistantScreen.kt
@@ -1,51 +1,11 @@
 package com.muammarahlnn.asco.feature.adminpracticumcreate.component.selectassistant
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Check
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.muammarahlnn.asco.core.designsystem.theme.AscoTheme
-import com.muammarahlnn.asco.core.designsystem.theme.DarkBlue
-import com.muammarahlnn.asco.core.designsystem.theme.DarkerPurple
-import com.muammarahlnn.asco.core.designsystem.theme.Gray
-import com.muammarahlnn.asco.core.designsystem.theme.PureWhite
-import com.muammarahlnn.asco.core.designsystem.theme.Purple
-import com.muammarahlnn.asco.core.designsystem.theme.White
-import com.muammarahlnn.asco.core.ui.component.BaseCard
-import com.muammarahlnn.asco.core.ui.component.OutlinedSearchBar
-import com.muammarahlnn.asco.feature.adminpracticumcreate.R
+import com.muammarahlnn.asco.core.ui.component.SelectMembersScreen
 
 /**
  * @Author Muammar Ahlan Abimanyu
@@ -57,125 +17,11 @@ internal fun SelectAssistantScreen(
     onSearchQueryChange: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(
-        contentPadding = PaddingValues(16.dp),
+    SelectMembersScreen(
+        searchQuery = searchQuery,
+        onSearchQueryChange = onSearchQueryChange,
         modifier = modifier,
-    ) {
-        item {
-            OutlinedSearchBar(
-                query = searchQuery,
-                placeholder = stringResource(id = R.string.search_assistant_placeholder),
-                onQueryChange = onSearchQueryChange,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            )
-        }
-
-        items(5) {
-            SelectAssistantCard(
-                username = "Ardan",
-                fullName = "Muammar Ahlan Abimanyu",
-                batch = "2019",
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-        }
-    }
-}
-
-@Composable
-private fun SelectAssistantCard(
-    username: String,
-    fullName: String,
-    batch: String,
-    modifier: Modifier = Modifier,
-) {
-    var selected by remember { mutableStateOf(false) }
-    BaseCard(
-        onClick = { selected = !selected },
-        modifier = modifier,
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Image(
-                imageVector = Icons.Rounded.Person,
-                contentDescription = null,
-                modifier = Modifier.size(50.dp)
-            )
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            Column(
-                modifier = Modifier.weight(1f),
-            ) {
-                Text(
-                    text = username,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = Purple,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-                Text(
-                    text = fullName,
-                    style = MaterialTheme.typography.labelLarge.copy(
-                        fontWeight = FontWeight.SemiBold,
-                    ),
-                    color = DarkerPurple,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-                Spacer(modifier = Modifier.height(2.dp))
-                Text(
-                    text = batch,
-                    style = MaterialTheme.typography.bodySmall,
-                    color = Gray,
-                )
-            }
-
-            Spacer(modifier = Modifier.width(8.dp))
-
-            AnimatedContent(
-                targetState = selected,
-                label = "Selected icon animation"
-            ) { selected ->
-                if (selected) {
-                    Box(
-                        contentAlignment = Alignment.Center,
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .border(
-                                width = 1.dp,
-                                color = DarkerPurple,
-                                shape = CircleShape,
-                            )
-                            .background(Purple)
-                            .size(28.dp)
-                    ) {
-                        Icon(
-                            imageVector = Icons.Rounded.Check,
-                            contentDescription = null,
-                            tint = PureWhite,
-                            modifier = Modifier.padding(4.dp)
-                        )
-                    }
-                } else {
-                    Box(
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .border(
-                                width = 1.dp,
-                                color = DarkBlue,
-                                shape = CircleShape,
-                            )
-                            .background(White)
-                            .size(28.dp)
-                    )
-                }
-            }
-        }
-    }
+    )
 }
 
 @Preview
