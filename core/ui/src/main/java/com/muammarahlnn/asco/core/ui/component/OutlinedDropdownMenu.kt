72fc9fa5e11284fcc9da9e48ab6bdd286bb0d6f8
muammarahlnn <21muammarahlan@gmail.com>
2024-07-08 21:49:24 +0800

    feat(core:ui): add PurpleOutlinedDropdownMenu

--- core/ui/src/main/java/com/muammarahlnn/asco/core/ui/component/OutlinedDropdownMenu.kt
+++ core/ui/src/main/java/com/muammarahlnn/asco/core/ui/component/OutlinedDropdownMenu.kt
@@ -15,10 +15,13 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.muammarahlnn.asco.core.designsystem.theme.AscoTheme
 import com.muammarahlnn.asco.core.designsystem.theme.DarkBlue
+import com.muammarahlnn.asco.core.designsystem.theme.DarkerPurple
 import com.muammarahlnn.asco.core.designsystem.theme.Gray
 import com.muammarahlnn.asco.core.designsystem.theme.PureWhite
 
@@ -26,12 +29,59 @@ import com.muammarahlnn.asco.core.designsystem.theme.PureWhite
  * @Author Muammar Ahlan Abimanyu
  * @File GrayOutlinedDropdownMenu, 22/06/2024 23.20
  */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GrayOutlinedDropdownMenu(
     value: String,
     isExpanded: Boolean,
     menuItems: List<String>,
+    onItemClick: (Int, String) -> Unit,
+    onExpandedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    BaseOutlinedDropdownMenu(
+        value = value,
+        isExpanded = isExpanded,
+        menuItems = menuItems,
+        borderColor = DarkBlue,
+        borderRadius = 16.dp,
+        textColor = Gray,
+        onItemClick = onItemClick,
+        onExpandedChange = onExpandedChange,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun PurpleOutlinedDropdownMenu(
+    value: String,
+    isExpanded: Boolean,
+    menuItems: List<String>,
+    onItemClick: (Int, String) -> Unit,
+    onExpandedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    BaseOutlinedDropdownMenu(
+        value = value,
+        isExpanded = isExpanded,
+        menuItems = menuItems,
+        borderColor = DarkerPurple,
+        borderRadius = 8.dp,
+        textColor = DarkerPurple,
+        onItemClick = onItemClick,
+        onExpandedChange = onExpandedChange,
+        modifier = modifier,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BaseOutlinedDropdownMenu(
+    value: String,
+    isExpanded: Boolean,
+    menuItems: List<String>,
+    borderColor: Color,
+    borderRadius: Dp,
+    textColor: Color,
     onItemClick: (Int, String) -> Unit,
     onExpandedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
@@ -53,34 +103,34 @@ fun GrayOutlinedDropdownMenu(
                         Icon(
                             imageVector = Icons.Rounded.ArrowDropDown,
                             contentDescription = null,
-                            tint = DarkBlue,
+                            tint = borderColor,
                         )
                     } else {
                         Icon(
                             imageVector = Icons.Rounded.ArrowDropUp,
                             contentDescription = null,
-                            tint = DarkBlue,
+                            tint = borderColor,
                         )
                     }
                 }
             },
             readOnly = true,
             maxLines = 1,
-            shape = RoundedCornerShape(16.dp),
+            shape = RoundedCornerShape(borderRadius),
             colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = DarkBlue,
-                unfocusedBorderColor = DarkBlue,
-                focusedTextColor = Gray,
-                unfocusedTextColor = Gray,
+                focusedBorderColor = borderColor,
+                unfocusedBorderColor = borderColor,
+                focusedTextColor = textColor,
+                unfocusedTextColor = textColor,
                 focusedContainerColor = PureWhite,
                 unfocusedContainerColor = PureWhite,
             ),
-            modifier = Modifier.menuAnchor()
+            modifier = modifier.menuAnchor()
         )
 
         ExposedDropdownMenu(
             expanded = isExpanded,
-            onDismissRequest = { onExpandedChange(false) }
+            onDismissRequest = { onExpandedChange(false) },
         ) {
             menuItems.forEachIndexed { index, menu ->
                 DropdownMenuItem(
@@ -93,7 +143,7 @@ fun GrayOutlinedDropdownMenu(
                     },
                     onClick = {
                         onItemClick(index, menu)
-                    }
+                    },
                 )
             }
         }
